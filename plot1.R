55b5610a971c83fa5847ec7f170a7154bd266594
Hunter <hwinstonc@gmail.com>
2015-07-12 18:49:38 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,3 +5,7 @@ subpwrdata2<-pwrdata[pwrdata$Date == "2007-2-2",]
 subpwrdata<-rbind(subpwrdata1,subpwrdata2)
 hist(subpwrdata$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "red")
 
+## create png
+png(file="plot1.png")
+hist(subpwrdata$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "red")
+dev.off()
